d7237c11bd0a489ffe1dd2602c4895a0c8d8f9af
DiscoveryUnlimited <jason.d.pagan89@gmail.com>
2022-10-20 14:04:25 -0400

    Game info fixed AB#71

--- src/com.game.HauntedVillage/Engine.java
+++ src/com.game.HauntedVillage/Engine.java
@@ -1,5 +1,52 @@
 package com.game.HauntedVillage;
 
+import com.apps.util.Console;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import java.io.File;
+import java.io.IOException;
+
 class Engine {
 
+    public Engine() {
+    }
+
+    public void execute() {
+        Console.clear();
+        presentInfo();
+        Console.clear();
+    }
+
+
+    private void presentInfo() {
+
+        try (JsonParser jParser = new JsonFactory()
+                .createParser(new File("22.07.06-HauntedVillage/resources/info.json"))) {
+
+            // loop until token equal to "}"
+            while (jParser.nextToken() != JsonToken.END_OBJECT) {
+
+                String fieldname = jParser.getCurrentName();
+
+                if ("story".equals(fieldname)) {
+                    jParser.nextToken();
+                    System.out.println(jParser.getText());
+                }
+
+            }
+
+            Console.pause(3000);
+
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
