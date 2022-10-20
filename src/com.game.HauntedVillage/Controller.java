412a491d90e0514f24e3458d5759d0157b416316
Kevin Kash <kevin.kash@tlgcohort.com>
2022-10-20 13:00:20 -0700

    AB#70

--- src/com.game.HauntedVillage/Controller.java
+++ src/com.game.HauntedVillage/Controller.java
@@ -8,9 +8,6 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Controller {
     // FIELDS
     //private final Scanner scanner = new Scanner(System.in);
