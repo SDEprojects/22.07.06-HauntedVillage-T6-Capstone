1abbc6b7db626dabc8a85a4e9839bfd85a014007
Garcia <migelig@amazon.com>
2022-10-26 09:23:59 -0700

    updating ticket

--- src/com.game.HauntedVillage/NPC.java
+++ src/com.game.HauntedVillage/NPC.java
@@ -1,5 +1,18 @@
 package com.game.HauntedVillage;
 
+/*
+* NPC Characters:
+*
+* Northern Square: Children
+* Courtyard: Villagers
+* South Square: None
+* Farm: Warewolf, Farmer, and Scarecrow
+* Townhall:Clerk
+* Tavern: Unknown
+* Church: Pastor + Cat
+* Well: None
+* Woods: Demon
+ **/
 class NPC {
 
 }
