6aa88f679428f42340554f5af29d71f55980f80e
fabit0v <fabitov@amazon.com>
2022-11-03 10:09:00 -0700

    AB#850 refactored dialogue

--- src/com.game.HauntedVillage/ParseCommand.java
+++ src/com.game.HauntedVillage/ParseCommand.java
@@ -12,13 +12,13 @@ class ParseCommand {
     final List<String> dropCommand = new ArrayList<>(Arrays.asList("drop", "throw", "abandon"));
     final List<String> movingCommand = new ArrayList<>(Arrays.asList("go", "moving", "move", "walk", "run", "turn"));
     final List<String> attackCommand = new ArrayList<>(Arrays.asList("attack", "fight"));
-    final List<String> searchCommand = new ArrayList<>(Arrays.asList("search"));
-    final List<String> speakCommand = new ArrayList<>(Arrays.asList("speak","talk"));
+    //final List<String> searchCommand = new ArrayList<>(Arrays.asList("search"));
+    final List<String> speakCommand = new ArrayList<>(Arrays.asList("speak", "talk"));
     final List<String> direction = new ArrayList<>(Arrays.asList("south", "north", "east", "west"));
-    final List<String> musicControl=new ArrayList<>(Arrays.asList("play","stop","reset","lower","raise"));
+    final List<String> musicControl = new ArrayList<>(Arrays.asList("play", "stop", "reset", "lower", "raise"));
 
     private final List<List> listCommand = new ArrayList<>(Arrays.asList(getCommand, checkCommand,
-            lookCommand, useCommand, dropCommand, attackCommand,searchCommand, speakCommand));
+            lookCommand, useCommand, dropCommand, attackCommand, speakCommand));
 
     List command() {
         List<String> allCommand = new ArrayList<>();
@@ -41,9 +41,8 @@ class ParseCommand {
             return "drop";
         } else if (attackCommand.contains(commandParse)) {
             return "attack";
-        } else if (searchCommand.contains(commandParse)){
-            return "search";
-        }else if (speakCommand.contains(commandParse)) {
+
+        } else if (speakCommand.contains(commandParse)) {
             return "speak";
         }
         return null;
