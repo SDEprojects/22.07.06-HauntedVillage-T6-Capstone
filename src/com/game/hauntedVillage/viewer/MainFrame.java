072dc6ff415e9b883d2d71b0e9e22ce96a01e07a
fabit0v <fabitov@amazon.com>
2022-11-08 15:19:06 -0800

    AB#876 -added top panel class and image icons. -top panel renders but still need to implement icons

--- src/com/game/hauntedVillage/viewer/MainFrame.java
+++ src/com/game/hauntedVillage/viewer/MainFrame.java
@@ -8,24 +8,32 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private MainPanel backgroundPanel;
     private TextPanel textPanel;
+    private TopPanel topPanel;
 
-    public MainFrame(GameManager baseController){
-        backgroundPanel=new MainPanel(baseController);
-        textPanel=new TextPanel(baseController);
+    public static void main(String[] args) {
+
+    }
+
+    public MainFrame(GameManager baseController) {
+        backgroundPanel = new MainPanel(baseController);
+        textPanel = new TextPanel(baseController);
+        topPanel = new TopPanel(baseController);
         setupFrame();
     }
-    private void setupFrame(){
+
+    private void setupFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setSize(1000,1000);
+        this.setSize(1000, 1000);
         getContentPane().setBackground(Color.black);
 
         //add background panel to the main frame
-        for(JPanel backGround:backgroundPanel.getBgPanel()) {
+        for (JPanel backGround : backgroundPanel.getBgPanel()) {
             add(backGround);
         }
         //add textPanel on the main frame
         add(textPanel);
+        add(topPanel);
         this.setVisible(true);
     }
 }
