67adcba476b9b0054a8027d61fe91eebfcfae401
Vanessa Constant <vaconstant@gmail.com>
2022-11-08 16:23:21 -0500

    Created splashscreen, AB#679

--- src/com/game/hauntedVillage/viewer/MainFrame.java
+++ src/com/game/hauntedVillage/viewer/MainFrame.java
@@ -8,10 +8,11 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private MainPanel backgroundPanel;
     private TextPanel textPanel;
-
+    private SplashScreen splashScreen;
     public MainFrame(GameManager baseController){
         backgroundPanel=new MainPanel(baseController);
         textPanel=new TextPanel(baseController);
+        splashScreen=new SplashScreen(baseController);
         setupFrame();
     }
     private void setupFrame(){
@@ -19,13 +20,22 @@ public class MainFrame extends JFrame {
         this.setLayout(null);
         this.setSize(1000,1000);
         getContentPane().setBackground(Color.black);
+        boolean isBackTextVisible = false;
+        add(splashScreen);
+        System.out.println(splashScreen.getGameStatus());
 
-        //add background panel to the main frame
-        for(JPanel backGround:backgroundPanel.getBgPanel()) {
-            add(backGround);
-        }
-        //add textPanel on the main frame
-        add(textPanel);
+
+            //add background panel to the main frame
+            for (JPanel backGround : backgroundPanel.getBgPanel()) {
+                add(backGround);
+                backGround.setVisible(true);
+            }
+            //add textPanel on the main frame
+            add(textPanel);
+            textPanel.setVisible(true);
+
+
+
         this.setVisible(true);
     }
 }
